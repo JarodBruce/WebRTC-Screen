95999a4e9eab2e3a5048f415207687769deb595a
Noname <>
2025-09-24 01:38:02 +0900

    first commit

---
+++ controller/main.go
@@ -0,0 +1,171 @@
+package main
+
+import (
+	"bufio"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"image"
+	"io"
+	"os"
+	"strings"
+	"sync"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/pion/webrtc/v3"
+)
+
+const (
+	screenWidth  = 1280
+	screenHeight = 720
+)
+
+// Game structはEbitenのゲーム状態を管理します
+type Game struct {
+	imgLock sync.Mutex
+	img     *image.RGBA
+}
+
+// Updateはゲームのロジックを更新します (今回は入力イベントの送信に使う予定)
+func (g *Game) Update() error {
+	// このループでマウスやキーボードの入力を検知し、データチャネルで送信する
+	return nil
+}
+
+// Drawは画面を描画します
+func (g *Game) Draw(screen *ebiten.Image) {
+	g.imgLock.Lock()
+	defer g.imgLock.Unlock()
+	if g.img != nil {
+		screen.WritePixels(g.img.Pix)
+	}
+}
+
+// Layoutは画面サイズを決定します
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return screenWidth, screenHeight
+}
+
+func main() {
+	game := &Game{}
+
+	// --- WebRTC PeerConnectionのセットアップ ---
+	peerConnection, err := webrtc.NewPeerConnection(webrtc.Configuration{
+		ICEServers: []webrtc.ICEServer{
+			{
+				URLs: []string{"stun:stun.l.google.com:19302"},
+			},
+		},
+	})
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if cErr := peerConnection.Close(); cErr != nil {
+			fmt.Printf("cannot close peerConnection: %v\n", cErr)
+		}
+	}()
+
+	// --- ビデオトラックの受信設定 ---
+	peerConnection.OnTrack(func(track *webrtc.TrackRemote, receiver *webrtc.RTPReceiver) {
+		fmt.Printf("Track has started, of type %d: %s \n", track.PayloadType(), track.Codec().MimeType)
+		codecName := strings.Split(track.Codec().MimeType, "/")[1]
+		fmt.Printf("Codec: %s\n", codecName)
+
+		        // 受信したビデオフレームをデコードしてebitenの画像に変換するゴルーチン
+				go func() {
+					// ticker := new(time.Ticker) // This was unused
+					// This is a very basic example of how to handle frames.
+					// In a real-world application, you would want to use a more robust solution.
+					// For example, you would want to handle packet loss and out-of-order packets.
+					// You would also want to use a jitter buffer.
+					for {
+						_, _, readErr := track.ReadRTP()
+						if readErr != nil {
+							// handle error
+							return // Exit goroutine on error
+						}
+						// Frame handling logic will be implemented here.
+					}
+				}()
+			})
+		
+			// --- シグナリング (オファーの待機とアンサーの生成) ---
+			fmt.Println("Paste the Offer from the host below:")
+			offer := webrtc.SessionDescription{}
+			Decode(MustReadStdin(), &offer)
+		
+			if err = peerConnection.SetRemoteDescription(offer); err != nil {
+				panic(err)
+			}
+		
+			answer, err := peerConnection.CreateAnswer(nil)
+			if err != nil {
+				panic(err)
+			}
+		
+			// gatherComplete := webrtc.GatheringCompletePromise(peerConnection) // This was unused
+			if err = peerConnection.SetLocalDescription(answer); err != nil {
+				panic(err)
+			}
+			// <-gatherComplete // This was unused
+		
+			fmt.Println("--- Answer (copy this to the host) ---")
+			fmt.Println(Encode(*peerConnection.LocalDescription()))
+			fmt.Println("---------------------------------------")
+	// --- 接続状態の監視 ---
+	peerConnection.OnConnectionStateChange(func(s webrtc.PeerConnectionState) {
+		fmt.Printf("Peer Connection State has changed: %s\n", s.String())
+		if s == webrtc.PeerConnectionStateFailed {
+			fmt.Println("Peer Connection has failed. Exiting")
+			os.Exit(0)
+		}
+	})
+
+	// --- Ebitenウィンドウの起動 ---
+	ebiten.SetWindowSize(screenWidth, screenHeight)
+	ebiten.SetWindowTitle("WebLinuxGUI Controller")
+	if err := ebiten.RunGame(game); err != nil {
+		panic(err)
+	}
+}
+
+// --- シグナリング情報 (SDP) をエンコード/デコードするためのヘルパー関数 ---
+
+func Encode(obj interface{}) string {
+	b, err := json.Marshal(obj)
+	if err != nil {
+		panic(err)
+	}
+	return base64.StdEncoding.EncodeToString(b)
+}
+
+func Decode(in string, obj interface{}) {
+	b, err := base64.StdEncoding.DecodeString(in)
+	if err != nil {
+		panic(err)
+	}
+	err = json.Unmarshal(b, obj)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func MustReadStdin() string {
+	r := bufio.NewReader(os.Stdin)
+	var in string
+	for {
+		var err error
+		in, err = r.ReadString('\n')
+		if err != io.EOF {
+			if err != nil {
+				panic(err)
+			}
+		}
+		in = strings.TrimSpace(in)
+		if len(in) > 0 {
+			break
+		}
+	}
+	return in
+}
---
+++ host/main.go
@@ -0,0 +1,163 @@
+package main
+
+import (
+	"bufio"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"image"
+	"io"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/kbinani/screenshot"
+	"github.com/pion/webrtc/v3"
+	"github.com/pion/webrtc/v3/pkg/media"
+)
+
+func main() {
+	// --- WebRTC PeerConnectionのセットアップ ---
+	peerConnection, err := webrtc.NewPeerConnection(webrtc.Configuration{
+		ICEServers: []webrtc.ICEServer{
+			{
+				URLs: []string{"stun:stun.l.google.com:19302"},
+			},
+		},
+	})
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if cErr := peerConnection.Close(); cErr != nil {
+			fmt.Printf("cannot close peerConnection: %v\n", cErr)
+		}
+	}()
+
+	// --- ビデオトラックの作成 ---
+	videoTrack, err := webrtc.NewTrackLocalStaticSample(webrtc.RTPCodecCapability{MimeType: "video/vp8"}, "video", "pion")
+	if err != nil {
+		panic(err)
+	}
+	_, err = peerConnection.AddTrack(videoTrack)
+	if err != nil {
+		panic(err)
+	}
+
+	// --- シグナリング (オファーの生成とアンサーの待機) ---
+	offer, err := peerConnection.CreateOffer(nil)
+	if err != nil {
+		panic(err)
+	}
+	gatherComplete := webrtc.GatheringCompletePromise(peerConnection)
+	if err = peerConnection.SetLocalDescription(offer); err != nil {
+		panic(err)
+	}
+	<-gatherComplete
+
+	fmt.Println("--- Offer (copy this to the controller) ---")
+	fmt.Println(Encode(offer))
+	fmt.Println("-------------------------------------------")
+
+	fmt.Println("Paste the Answer from the controller below:")
+	answer := webrtc.SessionDescription{}
+	Decode(MustReadStdin(), &answer)
+
+	if err = peerConnection.SetRemoteDescription(answer); err != nil {
+		panic(err)
+	}
+
+	// --- 接続状態の監視 ---
+	peerConnection.OnConnectionStateChange(func(s webrtc.PeerConnectionState) {
+		fmt.Printf("Peer Connection State has changed: %s\n", s.String())
+		if s == webrtc.PeerConnectionStateFailed {
+			fmt.Println("Peer Connection has failed. Exiting")
+			os.Exit(0)
+		}
+	})
+
+	fmt.Println("Connection established! Starting screen capture...")
+
+	// --- 画面キャプチャとビデオ送信のループ ---
+	go func() {
+		ticker := time.NewTicker(33 * time.Millisecond) // ~30 FPS
+		for range ticker.C {
+			bounds := screenshot.GetDisplayBounds(0) // プライマリディスプレイを取得
+			img, err := screenshot.CaptureRect(bounds)
+			if err != nil {
+				fmt.Println("Error capturing screen:", err)
+				continue
+			}
+
+			// image.RGBA を image.YCbCr に変換 (VP8エンコーダが要求するフォーマット)
+			ycbcrImg := image.NewYCbCr(img.Bounds(), image.YCbCrSubsampleRatio420)
+			for y := 0; y < img.Bounds().Dy(); y++ {
+				for x := 0; x < img.Bounds().Dx(); x++ {
+					r, g, b, _ := img.At(x, y).RGBA()
+					// RGBA to YCbCr conversion
+					// Note: This is a simplified conversion. For production, use a proper library.
+					Y := (0.299*float64(r>>8) + 0.587*float64(g>>8) + 0.114*float64(b>>8))
+					Cb := 128 + (-0.168736*float64(r>>8) - 0.331264*float64(g>>8) + 0.5*float64(b>>8))
+					Cr := 128 + (0.5*float64(r>>8) - 0.418688*float64(g>>8) - 0.081312*float64(b>>8))
+					
+					ycbcrImg.Y[y*ycbcrImg.YStride+x] = uint8(Y)
+					if y%2 == 0 && x%2 == 0 {
+						uvIndex := (y/2)*ycbcrImg.CStride + (x/2)
+						ycbcrImg.Cb[uvIndex] = uint8(Cb)
+						ycbcrImg.Cr[uvIndex] = uint8(Cr)
+					}
+				}
+			}
+
+
+			if err := videoTrack.WriteSample(media.Sample{Data: ycbcrImg.Y, Duration: time.Second}); err != nil {
+				// This is a simplified way to send frames. A real implementation would need to handle frame timing and partitioning correctly.
+				// For now, we just log the error and continue.
+				// fmt.Println("Error writing sample:", err)
+			}
+		}
+	}()
+
+	// アプリケーションが終了しないように待機
+	select {}
+}
+
+// --- シグナリング情報 (SDP) をエンコード/デコードするためのヘルパー関数 ---
+
+func Encode(obj interface{}) string {
+	b, err := json.Marshal(obj)
+	if err != nil {
+		panic(err)
+	}
+	return base64.StdEncoding.EncodeToString(b)
+}
+
+func Decode(in string, obj interface{}) {
+	b, err := base64.StdEncoding.DecodeString(in)
+	if err != nil {
+		panic(err)
+	}
+	err = json.Unmarshal(b, obj)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func MustReadStdin() string {
+	r := bufio.NewReader(os.Stdin)
+	var in string
+	for {
+		var err error
+		in, err = r.ReadString('\n')
+		if err != io.EOF {
+			if err != nil {
+				panic(err)
+			}
+		}
+		in = strings.TrimSpace(in)
+		if len(in) > 0 {
+			break
+		}
+	}
+	return in
+}
